a340e9e7d4ff1ec26fc19a88bd905989576432d8
snchzzero <aaevstafev@gmail.com>
2022-06-19 04:30:08 +0300

    heroku

--- main.py
+++ main.py
@@ -25,19 +25,25 @@ def start_schedule(USER_ID):
     bot.send_message(USER_ID, message_string)
 
     schedule.every(10).seconds.do(send_message1)
-# schedule.every().day.at("11:02").do(send_message1)
+#schedule.every().day.at("11:02").do(send_message1)
     while True:  # Запуск цикла
         schedule.run_pending()
         time.sleep(1)
 
 def send_message1():
-    bot.send_message(USER_ID, 'Отправка сообщения по времени')
+    l1 = db_google_sheets()
+    message_string = "Cрок поставки прошел по следующим позициям: (сортировка по дате) \n"
+    for l in l1:
+        message_string += f"№{l[0]}, Заказ №{l[1]}, Срок: {l[2]} \n"
+    bot.send_message(USER_ID, message_string)
 
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    msg = bot.send_message(message.chat.id, f'Нажали start {message.chat.id}')
+    msg = bot.send_message(message.chat.id, f'Уведомления по срокам поставки - \n'
+                                            f'Включено (раз в час) \n'
+                                            f'для отмены введите "/stop"')
     bot.register_next_step_handler(msg, start_process(msg))
 
 @bot.message_handler(commands=['stop'])
@@ -58,4 +64,5 @@ if __name__ == '__main__':
         bot.polling(none_stop=True)
     except:
         pass
+
 
