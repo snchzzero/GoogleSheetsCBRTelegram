f4e53707a29bbaa1f730f1801489cb106a927af2
snchzzero <aaevstafev@gmail.com>
2022-06-19 05:02:58 +0300

    heroku deploy2 db_heroku

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 telegram_token = "5422681116:AAGuZF4ayBIX8l8FeO7OUEOSxNTZTFPpsNY"
 
 # для базы данных
-db_host = "127.0.0.1"
-db_user = "postgres"
-db_password = "123"
-db_name = "google_sheets"
+db_host = 'ec2-34-247-172-149.eu-west-1.compute.amazonaws.com' #"127.0.0.1"
+db_user = 'hnarssxqztnvfc' #"postgres"
+db_password = 'a3b0194b434122e114fc48121a3389d2cceedd283072a8459dfa862831dd3920' #"123"
+db_name = 'daugvuosiphmdc' #"google_sheets"
 db_port = "5432"
 
