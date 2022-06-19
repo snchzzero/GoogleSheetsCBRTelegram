2a2a537f255181d279645ae4cf9bfc83c235b5ec
snchzzero <aaevstafev@gmail.com>
2022-06-19 03:00:37 +0300

    before time.sleep

--- main.py
+++ main.py
@@ -3,34 +3,29 @@ import telebot
 import time
 import datetime
 from multiprocessing import *
-#from multiprocessing.context import Process
 import schedule
-#global USER_ID
-#USER_ID = 0
+from script import db_google_sheets
 
 bot =telebot.TeleBot(telegram_token)
-
-
-
-
-
-
-
-
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    msg = bot.send_message(message.chat.id, f'Нажали start {message.chat.id}')
-    bot.register_next_step_handler(msg, start_process(msg))
 
 def start_process(msg):  # Запуск Process
     global USER_ID
     USER_ID = msg.chat.id
+    global p1
     p1 = Process(target=start_schedule(USER_ID), args=()).start()
 
+
+
+
 def start_schedule(USER_ID):
+    l1 = db_google_sheets()
+    message_string = "Cрок поставки прошел по следующим позициям: (сортировка по дате) \n"
+    for l in l1:
+        message_string += f"№{l[0]}, Заказ №{l[1]}, Срок: {l[2]} \n"
+    bot.send_message(USER_ID, message_string)
+
     schedule.every(10).seconds.do(send_message1)
+# schedule.every().day.at("11:02").do(send_message1)
     while True:  # Запуск цикла
         schedule.run_pending()
         time.sleep(1)
@@ -39,62 +34,28 @@ def send_message1():
     bot.send_message(USER_ID, 'Отправка сообщения по времени')
 
 
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    msg = bot.send_message(message.chat.id, f'Нажали start {message.chat.id}')
+    bot.register_next_step_handler(msg, start_process(msg))
+
+@bot.message_handler(commands=['stop'])
+def start(message):
+    msg = bot.send_message(message.chat.id, f'Расылка уведомлений выключена')
+    #p1.terminate()
+    #p1.close()
+    p1.join()
+
+    #schedule.cancel_job(start_schedule(USER_ID))
+
+
+
+
+
 if __name__ == '__main__':
     try:
         bot.polling(none_stop=True)
     except:
         pass
-
-
-# schedule.every().day.at("11:02").do(send_message1)
-# while True:  # Запуск цикла
-#     schedule.run_pending()
-#     time.sleep(1)
-
-#
-# telebot.apihelper.proxy = {'PROXY'}
-#
-#
-#
-#
-# def start_process():  # Запуск Process
-#     p1 = Process(target=P_schedule.start_schedule, args=()).start()
-#
-#
-# class P_schedule():  # Class для работы с schedule
-#     def start_schedule():  # Запуск schedule
-#         ######Параметры для schedule######
-#         schedule.every().day.at("11:02").do(P_schedule.send_message1)
-#         schedule.every(1).minutes.do(P_schedule.send_message2)
-#         ##################################
-#
-#         while True:  # Запуск цикла
-#             schedule.run_pending()
-#             time.sleep(1)
-#
-#     ####Функции для выполнения заданий по времени
-#     def send_message1():
-#         bot.send_message(USER_ID, 'Отправка сообщения по времени')
-#
-#     def send_message2():
-#         bot.send_message(USER_ID, 'Отправка сообщения через определенное время')
-#     ################
-
-
-# ###Настройки команд telebot#########
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#     USER_ID = message.from_user.id
-#     bot.send_message(message.chat.id, 'Нажали start')
-
-
-#####################
-
-#
-# if __name__ == '__main__':
-#     start_process()
-#     try:
-#         bot.polling(none_stop=True)
-#     except:
-#         pass
 
---
+++ script.py
@@ -0,0 +1,133 @@
+# для google_API
+from pprint import pprint
+import httplib2
+from googleapiclient import discovery  # вместо apiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
+
+# для db_google_sheets
+import psycopg2
+from config import db_host, db_user, db_password, db_name
+import requests
+import xmltodict
+
+# для логов
+from datetime import datetime
+import sys
+import time
+import sys
+
+# функция чтения гугл таблицы
+def google_API():
+    #подключение API
+    
+    CREDENTIALS_FILE = 'creds.json'  # файл с API
+    spreadsheet_id = '1V-lsTLgKAZ7Kn90ARq3K84YsQxkD-h-mML2Xl0VATD8' # из url схемы таблицы гугл (тестовое)
+    
+    # документы с которыми будем работать
+    creadentials = ServiceAccountCredentials.from_json_keyfile_name(
+        CREDENTIALS_FILE,
+        ['https://www.googleapis.com/auth/spreadsheets',
+         'https://www.googleapis.com/auth/drive'])
+    
+    # создаем объект аунтификации
+    httpAuth = creadentials.authorize(httplib2.Http())
+    
+    #создаем обертку API из которой мы будем получать данные из нашей схемы (v4 версия API sheets)
+    service = discovery.build('sheets', 'v4', http=httpAuth)
+       
+    #Читаем данные 'A1:aA10' - диапозон, если весь то range='Лист1'
+    values = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id,
+        range='Лист1',
+        majorDimension='ROWS').execute()
+    #pprint(values)
+    return (values) 
+    #exit()
+
+# функция текущего курса долара
+def cbr_usd_api():
+    day = datetime.now().strftime("%d")
+    mounth = datetime.now().strftime("%m")
+    year = datetime.now().strftime("%Y")
+    url = f'http://www.cbr.ru/scripts/XML_daily.asp?date_req={day}/{mounth}/{year}'
+    xml = xmltodict.parse(requests.get(url).content)
+
+    for value in xml['ValCurs']['Valute']:
+        if value['@ID'] == 'R01235':
+            #print(value['Name'])
+            return(value['Value'])
+
+#функция записи данных в БД PostgreSQL
+def db_google_sheets():
+    now = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    try:
+        #подключаемся к базе данных
+        #создаем объект
+        connection = psycopg2.connect(
+            host=db_host,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+        connection.autocommit = True  # что бы не писать после каждого запроса коммит
+
+        #для работы с БД нужно создать объект курсор (для выполнения различных команд SQl)
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            DROP TABLE IF EXISTS test;""")
+            print(f'[{now}]: Drop old table "test" successfully')
+    
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            CREATE TABLE test(
+            id serial PRIMARY KEY,
+            number integer,
+            order_number integer,
+            value_dolar int,
+            value_rub int,
+            delivery_time date);""")
+            print(f'[{now}]: Create new table "test" successfully')
+
+        values = google_API()
+        print(f'[{now}]: Getting values Google Sheets successfully')
+
+        usd = float(str(cbr_usd_api()).replace(',', '.'))
+        print(f'[{now}]: Dollar rate {usd} rub')
+
+        for i in range(1, len(values['values'])):
+            number = values['values'][i][0]
+            order_number = values['values'][i][1]
+            value_dolar  = values['values'][i][2]
+            value_rub = round(usd * int(value_dolar), 0)
+            delivery_time  = values['values'][i][3]
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                INSERT INTO test (number, order_number, value_dolar, value_rub, delivery_time) VALUES
+                (%s, %s, %s, %s, %s);""",[int(number), int(order_number),
+                                          int(value_dolar), value_rub, delivery_time])
+        print(f'[{now}]: Insert values into table "test" successfully')
+
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            SELECT test.number, order_number, delivery_time
+            FROM test
+            WHERE delivery_time < CURRENT_DATE
+            ORDER BY delivery_time""")
+            l1 = cursor.fetchall()
+            return (l1)
+            # print(l1)
+            # for l in l1:
+            #     print("Значения", l)
+            #     print("*", l[1])
+            #     print()
+
+
+    except Exception as _ex:
+        #обработка ошибок
+        print(f'[{now}]: Error while working with PostgreSQL', _ex)
+    finally:
+        #закрываем подключение к БД
+        if connection:
+            connection.close()
+            print(f'[{now}]: PostgreSQL connection closed')
+
